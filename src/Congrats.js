3a3113e8c85311af1d4d01d7fb7402ec046e00b8
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-28 22:52:14 +0200

    Testing Congrats component [red]

---
+++ src/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * Functional React component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+
+export default function Congrats({ success }) {
+  return <div />;
+}
