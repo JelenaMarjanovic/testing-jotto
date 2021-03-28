c0541d295603bfee7f225c1036c9fb5348b8f179
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-29 00:18:15 +0200

    Testing Congrats component [green]

--- src/Congrats.js
+++ src/Congrats.js
@@ -6,7 +6,20 @@ import React from 'react';
  * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
  */
+const Congrats = (props) => {
+  const { success } = props;
 
-export default function Congrats({ success }) {
-  return <div />;
-}
+  if (success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
+
+export default Congrats;
