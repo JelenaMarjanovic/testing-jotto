8a4f0521fac22faca0a774d73c3fd603b4473ca6
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-28 21:23:11 +0200

    Centralize util function for testingpurpose

---
+++ test/testUtils.js
@@ -0,0 +1,8 @@
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - Enzyme ShallowWrapper.
+ * @param {string} attrVal - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, attrVal) =>
+  wrapper.find(`[data-test='${attrVal}']`);
