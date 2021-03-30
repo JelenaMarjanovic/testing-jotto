9dc472598f6076f16455715bd827cd508cc61fee
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-03-30 16:19:30 +0200

    Input Component Props

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
+
+const defaultProps = {
+  secretWord: 'party'
+};
 
 /**
  * Factory function to create a ShallowWrapper for the Input component.
  * @returns {ShallowWrapper}
  */
-
-const setup = () => {
-  return shallow(<Input />);
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 test('renders without error', () => {
   const wrapper = setup();
   const inputComponent = findByTestAttr(wrapper, 'component-input');
   expect(inputComponent.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+  checkProps(Input, defaultProps);
 });
