2b1f290097d0ea9e10b1a36403b681cb0def6c6f
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-05 13:27:32 +0200

    State-Controlled Field Tests [red]

--- src/Input.test.js
+++ src/Input.test.js
@@ -26,3 +26,18 @@ test('renders without error', () => {
 test('does not throw warning with expected props', () => {
   checkProps(Input, defaultProps);
 });
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    const wrapper = setup();
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+});
