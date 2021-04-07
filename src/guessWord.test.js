0a828c2d7f81bdda305ff6b4eb315e27e4df3464
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-08 00:40:09 +0200

    Set up Functional Tests

---
+++ src/guessWord.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import { findByTestAttr } from '../test/testUtils';
+import App from './App';
+
+/**
+ * Create wrapper with specified initial conditions,
+ * then submit a guessed word of 'train'
+ * @function setup
+ * @param {object} state - Initial conditions.
+ * @returns {Wrapper} - Enzyme wrapper of mounted App component.
+ */
+const setup = (state = {}) => {
+  // TODO: apply state
+  const wrapper = mount(<App />);
+
+  // Add value to input box
+  const inputBox = findByTestAttr(wrapper, 'input-box');
+  inputBox.simulate('change', { target: { value: 'train' } });
+
+  // Simulate click on submit button
+  const submitButton = findByTestAttr(wrapper, 'submit-button');
+  submitButton.simulate('click', { preventDefault() {} });
+
+  return wrapper;
+};
+
+describe('no words guessed', () => {});
+
+describe('some words guessed', () => {});
+
+describe('guess secret word', () => {});
