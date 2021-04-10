9b087dd7cb1db69b909e6fa24b712ecc52773be3
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-10 19:34:43 +0200

    'successReducer' Test [green]

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,11 +1,18 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function successReducer
  * @param {array} state - Array of guessed words.
  * @param {object} action - Action to be reduced.
  * @returns {boolean} - New success state.
  */
-const successReducer = (state, action) => {
-  return null;
+const successReducer = (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
 
 export default successReducer;
