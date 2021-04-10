6d5bf4341734bcd799c8425a3388ed30b3d7c615
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-10 04:35:52 +0200

    Write "getSecretWord" Action [green]

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const getSecretWord = () => {
-  // Return response from server
+  // TODO: write actual action with Redux / Context
+  return axios.get('http://localhost:3030').then((response) => response.data);
 };
