f8c260127335a12d9d349965356503b6f075adee
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-10 16:13:33 +0200

    Set up Redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - Action to be reduced.
+ * @returns {boolean} - New success state.
+ */
+const successReducer = (state, action) => {
+  return null;
+};
+
+export default successReducer;
