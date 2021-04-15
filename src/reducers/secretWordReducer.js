8605e9d9b7ccda7fade7337f399822a3d39fe746
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-15 15:56:22 +0200

    Add 'secretWordReducer' [red]

---
+++ src/reducers/secretWordReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function secretWordReducer
+ * @param {array} state - A secret word.
+ * @param {object} action - Action to be reduced.
+ * @returns {array} - New secretWord state.
+ */
+const secretWord = (state = '', action) => {
+  return state;
+};
+
+export default secretWord;
