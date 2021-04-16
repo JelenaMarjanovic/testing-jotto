f39f1a0fc93eaefb64d50c99126a17ffad220e3c
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-16 13:23:47 +0200

    Write Action Creator and Reducer for secretWord

--- src/reducers/secretWordReducer.js
+++ src/reducers/secretWordReducer.js
@@ -1,11 +1,18 @@
+import { actionTypes } from '../actions';
+
 /**
  * @function secretWordReducer
- * @param {array} state - A secret word.
- * @param {object} action - Action to be reduced.
- * @returns {array} - New secretWord state.
+ * @param {string} state - State before reducer.
+ * @param {object} action - Action sent to reducer.
+ * @returns {string} - New state (secret word payload from action).
  */
 const secretWord = (state = '', action) => {
-  return state;
+  switch (action.type) {
+    case actionTypes.SET_SECRET_WORD:
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default secretWord;
