52a1c7fcea52c5ca3227018961bc94cb52a7a37c
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-19 01:07:28 +0200

    Challenge 2: Reset Game [reducers]

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -1,20 +1,26 @@
 import { actionTypes } from '../actions';
 import successReducer from './successReducer';
 
-test('when previous state is undefined, return `false`', () => {
+test('when previous state is undefined, returns `false`', () => {
   const newState = successReducer(undefined, {});
 
   expect(newState).toBe(false);
 });
 
-test('return previous state when unknown action type', () => {
+test('returns previous state when unknown action type', () => {
   const newState = successReducer(false, { type: 'unknown' });
 
   expect(newState).toBe(false);
 });
 
-test('return `true` for action type CORRECT_GUESS', () => {
+test('returns `true` for action type "CORRECT_GUESS"', () => {
   const newState = successReducer(false, { type: actionTypes.CORRECT_GUESS });
 
   expect(newState).toBe(true);
+});
+
+test('returns state of false upon receiving an action of type "RESET_GAME"', () => {
+  // Start with success true, since success is false by default
+  const newState = successReducer(true, { type: actionTypes.RESET_GAME });
+  expect(newState).toBe(false);
 });
