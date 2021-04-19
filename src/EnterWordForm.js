583a777ee88a4b392398b61392867ff7285572a6
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-19 16:57:14 +0200

    Challenge 4: EnterWordForm [component]

---
+++ src/EnterWordForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import './EnterWordForm.css';
+
+const EnterWordForm = ({ formAction }) => {
+  const [secretWord, setSecretWord] = useState('');
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    // Don't submit an empty word
+    if (secretWord.length > 0) {
+      formAction(secretWord);
+    }
+  };
+
+  return (
+    <div data-test="component-enter-word-form">
+      <p data-test="enter-word-instructions">
+        Enter a secret word for someone else to guess!
+      </p>
+      <form className="form-inline">
+        <input
+          data-test="enter-word-input"
+          className="mb-2 mx-sm-3"
+          type="text"
+          value={secretWord}
+          placeholder="Enter secret word"
+          onChange={(event) => setSecretWord(event.target.value)}
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+          onClick={submitForm}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+EnterWordForm.propTypes = {
+  formAction: PropTypes.func
+};
+
+export default EnterWordForm;
