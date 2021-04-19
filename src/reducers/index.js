b43365c17c44122f99c38f10a2931dca595b0b66
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-04-19 15:41:51 +0200

    Challenge 4: User Inputs Secret Word [reducers]

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,10 +4,12 @@ import success from './successReducer';
 import guessedWords from './guessedWordsReducer';
 import secretWord from './secretWordReducer';
 import giveUp from './giveUpReducer';
+import userEnter from './userEnterReducer';
 
 export default combineReducers({
   success,
   guessedWords,
   secretWord,
-  giveUp
+  giveUp,
+  userEnter
 });
